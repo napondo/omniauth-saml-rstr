fd8942646b377a01a8226b4645350a0049ebf111
Nevin A <nevin.n.apondo@nasa.gov>
2016-11-18 14:35:28 -0500

    Removing newlines

--- lib/omniauth/strategies/saml-rstr/auth_request.rb
+++ lib/omniauth/strategies/saml-rstr/auth_request.rb
@@ -22,14 +22,31 @@ module OmniAuth
                                   AssertionConsumerServiceURL=\"#{settings[:assertion_consumer_service_url]}\"
                                   xmlns:saml='urn:oasis:names:tc:SAML:2.0:assertion'
                                   xmlns:samlp='urn:oasis:names:tc:SAML:2.0:protocol'>"+
-                "<saml:Issuer>#{settings[:issuer]}</saml:Issuer>\n" +
-                "<samlp:NameIDPolicy Format=\"#{settings[:name_identifier_format]}\" AllowCreate=\"true\"/>\n" +
+                "<saml:Issuer>#{settings[:issuer]}</saml:Issuer>" +
+                "<samlp:NameIDPolicy Format=\"#{settings[:name_identifier_format]}\" AllowCreate=\"true\"/>" +
                 "<samlp:RequestedAuthnContext Comparison=\"exact\">" +
                     "<saml:AuthnContextClassRef>
                         urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport
                     </saml:AuthnContextClassRef>
-                </samlp:RequestedAuthnContext>\n" +
+                </samlp:RequestedAuthnContext>" +
             "</samlp:AuthnRequest>"
+
+            "<samlp:AuthnRequest AssertionConsumerServiceURL='https://developer.earthdata.nasa.gov/saml/acs'
+                                 Destination='https://auth.launchpad-sbx.nasa.gov/affwebservices/public/saml2sso'
+                                 ID='_c8122219-636c-4d7b-9fe5-0b5e6be4f930'
+                                 IssueInstant='2016-11-17T20:34:07Z'
+                                 ProtocolBinding='urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST'
+                                 Version='2.0'
+                                 xmlns:saml='urn:oasis:names:tc:SAML:2.0:assertion'
+                                 xmlns:samlp='urn:oasis:names:tc:SAML:2.0:protocol'>
+             <saml:Issuer>https://developer.earthdata.nasa.gov/saml/acs</saml:Issuer>
+             <samlp:NameIDPolicy AllowCreate='true' Format='urn:oasis:names:tc:SAML:2.0:nameid-format:transient'/>
+             <samlp:RequestedAuthnContext Comparison='exact'>
+                <saml:AuthnContextClassRef>
+                      urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport
+                </saml:AuthnContextClassRef>
+              </samlp:RequestedAuthnContext>
+            </samlp:AuthnRequest>"
 
           deflated_request  = Zlib::Deflate.deflate(request, 9)[2..-5]
           base64_request    = Base64.encode64(deflated_request)
